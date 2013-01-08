5949faacf73047dc05233bd051171d8bc84f4f60
fanabalia <fanabalia@gmail.com>
2013-01-08 21:50:03 +0100

    Añadir nueva clase ejercicio 1

---
+++ Ejercicio1Tema15/NuevaClase.h
@@ -0,0 +1,13 @@
+//
+//  NuevaClase.h
+//  Ejercicio1Tema15
+//
+//  Created by macbook on 08/01/13.
+//  Copyright (c) 2013 macbook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NuevaClase : NSObject
+
+@end
---
+++ Ejercicio1Tema15/NuevaClase.m
@@ -0,0 +1,13 @@
+//
+//  NuevaClase.m
+//  Ejercicio1Tema15
+//
+//  Created by macbook on 08/01/13.
+//  Copyright (c) 2013 macbook. All rights reserved.
+//
+
+#import "NuevaClase.h"
+
+@implementation NuevaClase
+
+@end
